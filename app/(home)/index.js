8fcff378b1ececbe7a6d0a81db2c261f92ce9da5
Ajaybanu <ajaybanu7@gmail.com>
2024-02-09 18:44:10 +0530

    add seach and create carousel

--- app/(home)/index.js
+++ app/(home)/index.js
@@ -1,92 +1,138 @@
-import { Alert, StyleSheet, Text, View, ScrollView } from 'react-native'
-import React, { useEffect, useState } from 'react'
+import {
+  Alert,
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Pressable,
+  TextInput,
+} from "react-native";
+import React, { useEffect, useState } from "react";
 import * as Location from "expo-location";
 import * as LocationGeocoding from "expo-location";
-import { SimpleLineIcons } from '@expo/vector-icons';
-
+import { SimpleLineIcons } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
 const Index = () => {
-    const [locationServicesEnabled, setLocationServicesEnabled] = useState(false);
-    const [displayCurrentAddress, setDisplayCurrentAddress] = useState("fetching your location ...");
-    const [data, setData] = useState([]);
+  const [locationServicesEnabled, setLocationServicesEnabled] = useState(false);
+  const [displayCurrentAddress, setDisplayCurrentAddress] = useState(
+    "fetching your location ..."
+  );
+  const [data, setData] = useState([]);
 
-    useEffect(() => {
-        CheckIfLocationEnabled();
-    }, []);
+  useEffect(() => {
+    CheckIfLocationEnabled();
+    GetCurrentLocation();
+  }, []);
 
-    useEffect(() => {
-        if (locationServicesEnabled) {
-            GetCurrentLocation();
-        }
-    }, [locationServicesEnabled]);
+  const CheckIfLocationEnabled = async () => {
+    let enabled = await Location.hasServicesEnabledAsync();
 
-    const CheckIfLocationEnabled = async () => {
-        let enabled = await Location.hasServicesEnabledAsync();
+    if (!enabled) {
+      Alert.alert(
+        "Location Services not enabled",
+        "Please enable your location services to continue",
+        [{ text: "OK" }],
+        { cancelable: false }
+      );
+    } else {
+      setLocationServicesEnabled(true);
+    }
+  };
 
-        if (!enabled) {
-            Alert.alert(
-                "Location Services not enabled",
-                "Please enable your location services to continue",
-                [{ text: "OK" }],
-                { cancelable: false }
-            );
-        } else {
-            setLocationServicesEnabled(true);
-        }
-    };
+  const GetCurrentLocation = async () => {
+    let { status } = await Location.requestBackgroundPermissionsAsync();
 
-    const GetCurrentLocation = async () => {
-        let { status } = await Location.requestBackgroundPermissionsAsync();
+    if (status !== "granted") {
+      Alert.alert(
+        "Permission not granted",
+        "Allow the app to use the location service",
+        [{ text: "OK" }],
+        { cancelable: false }
+      );
+    }
 
-        if (status !== "granted") {
-            Alert.alert(
-                "Permission not granted",
-                "Allow the app to use the location service",
-                [{ text: "OK" }],
-                { cancelable: false }
-            );
-        }
+    const location = await Location.getCurrentPositionAsync({
+      accuracy: Location.Accuracy.High,
+    });
+    console.log(location);
 
-        const location = await Location.getCurrentPositionAsync({
-            accuracy: Location.Accuracy.High,
-        });
+    let { coords } = await Location.getCurrentPositionAsync();
+    if (coords) {
+      const { latitude, longitude } = coords;
 
-        let { coords } = location;
-        if (coords) {
-            const { latitude, longitude } = coords;
+      let response = await Location.reverseGeocodeAsync({
+        latitude,
+        longitude,
+      });
 
-            let response = await Location.reverseGeocodeAsync({
-                latitude,
-                longitude,
-            });
+      const address = await LocationGeocoding.reverseGeocodeAsync({
+        latitude,
+        longitude,
+      });
 
-            const address = await LocationGeocoding.reverseGeocodeAsync({
-                latitude,
-                longitude,
-            });
+      const streetAddress = address[0].name;
+      for (let item of response) {
+        let address = `${item.name}, ${item?.postalCode}, ${item?.city}`;
+        setDisplayCurrentAddress(address);
+      }
+    }
+  };
 
-            const streetAddress = address[0].name;
-            let formattedAddress = "";
-            for (let item of response) {
-                formattedAddress = `${item.name}, ${item?.postalCode}, ${item?.city}`;
-            }
-            setDisplayCurrentAddress(formattedAddress);
-        }
-    };
+  console.log("my address", displayCurrentAddress);
 
-    console.log(displayCurrentAddress);
+  return (
+    <ScrollView style={{ flex: 1, backgroundColor: "#f8f8f8" }}>
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          gap: 12,
+          paddding: 10,
+        }}
+      >
+        <SimpleLineIcons name="location-pin" size={24} color="black" />
+        <View style={{ flex: 1 }}>
+          <Text style={{ fontSize: 15, fontWeight: "500" }}>
+            Delivery To ....
+          </Text>
+          <Text style={{ color: "gray", fontSize: 16, marginTop: 3 }}>
+            {displayCurrentAddress}
+          </Text>
+        </View>
+        <Pressable
+          style={{
+            backgroundColor: "#6CB4EE",
+            width: 40,
+            height: 40,
+            borderRadius: 20,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text>press</Text>
+        </Pressable>
+      </View>
 
-    return (
-        <ScrollView>
-            <View>
-                <SimpleLineIcons name="location-pin" size={24} color="black" />
-                <View>
-                    <Text>Delivery To ....</Text>
-                    <Text>{displayCurrentAddress}</Text>
-                </View>
-            </View>
-        </ScrollView>
-    )
-}
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between",
+          borderWidth: 1,
+          borderColor: "#C0C0C0",
+          paddingVertical: 8,
+          paddingHorizontal: 10,
+          borderRadius: 11,
+          marginTop: 10,
+          marginHorizontal: 10,
+        }}
+      >
+        <TextInput placeholder="Search for food, hotels" />
+        <AntDesign name="search1" size={24} color="#E52850" />
+      </View>
+    </ScrollView>
+  );
+};
 
 export default Index;
 
