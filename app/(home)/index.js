47d8a1e8b5a7a92381433e1a41c7a24dffbcbec6
Ajaybanu <ajaybanu7@gmail.com>
2024-02-10 22:14:24 +0530

    create categories and list Recommended items

--- app/(home)/index.js
+++ app/(home)/index.js
@@ -6,12 +6,15 @@ import {
   ScrollView,
   Pressable,
   TextInput,
+  Image,
 } from "react-native";
 import React, { useEffect, useState } from "react";
 import * as Location from "expo-location";
 import * as LocationGeocoding from "expo-location";
 import { SimpleLineIcons } from "@expo/vector-icons";
-import { AntDesign } from "@expo/vector-icons";
+import { AntDesign, Ionicons } from "@expo/vector-icons";
+import Carousel from "../../components/Carousel";
+import Categories from "../../components/Categories";
 const Index = () => {
   const [locationServicesEnabled, setLocationServicesEnabled] = useState(false);
   const [displayCurrentAddress, setDisplayCurrentAddress] = useState(
@@ -80,6 +83,49 @@ const Index = () => {
 
   console.log("my address", displayCurrentAddress);
 
+  const recommended = [
+    {
+      id: 0,
+      name: "Nandhana Palace",
+      image:"https://img.freepik.com/free-photo/exploding-burger-with-vegetables-melted-cheese-black-background-generative-ai_157027-1734.jpg",
+      time: "35 - 45",
+      type: "Andhra",
+    },
+    {
+      id: 0,
+      name: "GFC Biriyani",
+      image:
+        "https://cdn.pixabay.com/photo/2017/11/25/17/17/sandwich-2977251_640.jpg",
+      time: "10 - 35",
+      type: "North Indian",
+    },
+    {
+      id: 0,
+      name: "Happiness Dhaba",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrcFHU-IUkmyI5-drUFbyZsiRlb0Q6Zb-kmQ&usqp=CAU",
+      time: "20 - 25",
+      type: "North Indian",
+    },
+
+    {
+      id: 0,
+      name: "Happiness Dhaba",
+      image:
+        "https://images.alphacoders.com/862/862638.jpg",
+      time: "20 - 25",
+      type: "North Indian",
+    },
+    {
+      id: 0,
+      name: "Happiness Dhaba",
+      image:
+        "https://wallpapers.com/images/featured/delicious-food-pictures-i5wjpvjqrk3qroy0.jpg",
+      time: "20 - 25",
+      type: "North Indian",
+    },
+  ];
+
   return (
     <ScrollView style={{ flex: 1, backgroundColor: "#f8f8f8" }}>
       <View
@@ -130,6 +176,57 @@ const Index = () => {
         <TextInput placeholder="Search for food, hotels" />
         <AntDesign name="search1" size={24} color="#E52850" />
       </View>
+      <Carousel />
+
+      <Categories />
+
+      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+        {recommended?.map((item, index) => (
+          <View
+            style={{
+              backgroundColor: "white",
+              flexDirection: "row",
+              margin: 10,
+              borderRadius: 8,
+            }}
+          >
+            <View>
+              <Image
+                style={{
+                  width: 100,
+                  height: 100,
+                  resizeMode: "cover",
+                  borderTopLeftRadius: 8,
+                  borderBottomLeftRadius: 7,
+                }}
+                source={{ uri: item?.image }}
+              />
+            </View>
+            <View style={{ padding: 10, flexDirection: "column" }}>
+              <Text style={{ fontSize: 15, fontWeight: "500" }}>
+                {item?.name}
+              </Text>
+              <Text
+                style={{
+                  flex: 1,
+                  marginTop: 3,
+                  color: "gray",
+                  fontWeight: "500",
+                }}
+              >
+                {item?.type}
+              </Text>
+
+              <View
+                style={{ flexDirection: "row", alignItems: "center", gap: 3 }}
+              >
+                <Ionicons name="ios-time" size={24} color="green" />
+                <Text>{item?.time} mins</Text>
+              </View>
+            </View>
+          </View>
+        ))}
+      </ScrollView>
     </ScrollView>
   );
 };
--- components/Carousel.js
+++ components/Carousel.js
@@ -1,14 +1,32 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { SliderBox } from "react-native-image-slider-box";
 
 const Carousel = () => {
+  const images = [
+   
+    "https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg",
+    "https://images.pexels.com/photos/2474661/pexels-photo-2474661.jpeg?cs=srgb&dl=pexels-marvin-ozz-2474661.jpg&fm=jpg"
+   
+  ];
   return (
     <View>
-      <Text>Carousel</Text>
+       <SliderBox
+        images={images}
+        autoPlay
+        circleLoop
+        dotColor="#13274F"
+        inactiveDotColor="#90A4AE"
+        ImageComponentStyle={{
+            borderRadius:6,
+            width:"94%",
+            marginTop:10
+        }}
+      />
     </View>
-  )
-}
+  );
+};
 
-export default Carousel
+export default Carousel;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
